a3f6e1528c440fb539ba138f87272612aeccbe87
Meng-Yuan Huang <myh@live.com>
2022-04-22 09:07:12 +0800

    0.1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
 import React from 'react';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+import { useTranslation } from 'react-i18next';
+import { Provider, useDispatch, useSelector } from 'react-redux';
+import i18n from './i18n';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+interface MyState {
+  value: number;
+}
+
+const initialState: MyState = {
+  value: 0
+};
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    inc: s => {
+      s.value += 1;
+    },
+    dec: s => {
+      s.value -= 1;
+    }
+  }
+});
+
+const { inc, dec } = counterSlice.actions;
+
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
+
+function _App() {
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const count = useSelector<MyState>(s => s.value);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Count: {count}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button style={{fontSize: '32px'}} onClick={() => {
+          i18n.changeLanguage(i18n.language === 'en' ? 'zh' : 'en');
+          dispatch(inc());
+        }}>{t('Click')}</button>
       </header>
     </div>
   );
+}
+
+function App() {
+  return <Provider store={store}>
+    <_App />
+  </Provider>;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import i18n from './i18n';
+
+createRoot(
+  document.getElementById('root')!
+).render(<React.StrictMode>
+  <App />
+</React.StrictMode>)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
@@ -20,4 +22,4 @@ serviceWorkerRegistration.unregister();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
